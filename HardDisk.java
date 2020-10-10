dd78217c79b2f2eacf41012909d3d89eaa91b9d2
001221lry <72534000+001221lry@users.noreply.github.com>
2020-10-10 16:35:02 +0800

    Update HardDisk.java

--- HardDisk.java
+++ HardDisk.java
@@ -6,14 +6,9 @@ public class HardDisk {
 	 int getAmount(){
 	  return amount;
 	 }
-	 int getspeed2(){
-		 return speed2;
-	 }
+	
 	 public void setAmount(int amount){
 	  this.amount=amount;
-	 }
-	 public void setspeed2(int speed2) {
-		 this.speed2=speed2;
 	 }
 	 
 	}
